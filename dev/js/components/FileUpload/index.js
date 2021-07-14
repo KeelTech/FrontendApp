134de043ce162297a109581b455653a1f4622db5
yash-getkeel <87320841+yash-getkeel@users.noreply.github.com>
2021-07-14 11:30:20 +0530

    File upload component (#12)
    * file upload component added
    
    * constructor fixes
    
    Co-authored-by: yash-getkeel <yash@airflash.co>

---
+++ dev/js/components/FileUpload/index.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import { fileUpload, fileUploadWrapper, selectedFileText, outerShell, innerShell, closeWrapper, fileButton, fileData } from './style.js';
+import CustomDropDown from '@components/CustomDropDown';
+
+class FileUpload extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedFile: null,
+            selectedFileType: null,
+            options: [
+                {
+                    value: 1,
+                    displayName: "Aadhar Card"
+                },
+                {
+                    value: 2,
+                    displayName: "PAN Card"
+                },
+                {
+                    value: 3,
+                    displayName: "Driving License"
+                },
+                {
+                    value: 4,
+                    displayName: "Other"
+                }
+            ]
+        };
+    }
+    close = () => {
+        this.props.fileUploadModalClosed({
+            selectedFile: this.state.selectedFile,
+            selectedFileType: this.state.selectedFileType,
+        })
+    }
+    onFileChange = event => {
+        this.setState({ selectedFile: event.target.files[0] });
+        if (event.target.files[0]) {
+            const formData = new FormData();
+            formData.append(
+                "myFile",
+                event.target.files[0],
+                event.target.files[0].name
+            );
+            this.props.fileUploaded(formData);
+        }
+    };
+    btnClickedUpload = () => {
+        document.getElementById("file").click();
+    }
+    handleOptionChange = (item) => {
+        this.setState({ selectedFileType: item });
+    }
+    fileData = () => {
+        if (this.state.selectedFile) {
+            return (
+                <span className={selectedFileText}>
+                    <b>{this.state.selectedFile.name}</b> is selected
+                </span>
+            );
+        }
+        else {
+            return (
+                <button onClick={this.btnClickedUpload} className={fileButton}>
+                    Select File
+                </button>
+            );
+        }
+    };
+    render() {
+        return (
+            <div className={outerShell}>
+                <span onClick={this.close} className={closeWrapper}>x</span>
+                <div className={innerShell}>
+                    <div className={fileUploadWrapper}>
+                        <input id="file" className={fileUpload} type="file" onChange={this.onFileChange} />
+                    </div>
+                    <div className={fileData}>{this.fileData()}</div>
+                    <CustomDropDown list={this.state.options} optionSelected={this.handleOptionChange}></CustomDropDown>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default FileUpload;
---
+++ dev/js/components/FileUpload/style.js
@@ -0,0 +1,89 @@
+import { css } from '@emotion/css';
+import { mobileScreenWidth } from '@constants';
+import { primaryFont } from '@constants'
+
+export const fileUpload = css`
+    opacity:0;
+    width:100%;
+    height:100%;
+    cursor:pointer
+`
+export const fileUploadWrapper = css`
+    width:95px;
+    height:95px;
+    background-image:url("${ASSETS_BASE_URL}/images/common/file.svg");
+    background-color:transparent;
+    background-repeat: no-repeat;
+    margin-left: auto;
+    margin-right: auto;
+`
+
+export const selectedFileText = css`
+    font-family: ${primaryFont};
+    font-style: normal;
+    font-weight: bold;
+    font-size: 16px;
+    line-height: 24px;
+    text-align: center;
+    color: #363B64;
+`
+export const alignCenter = css`
+    display:flex;
+    justify-content:center;
+    align-items:center
+`
+
+export const outerShell = css`
+    background: white;
+    position:absolute;
+    top:25%;
+    width:80vw;
+    max-width: 890px;
+    box-shadow: 0px 20px 50px rgba(191, 21, 108, 0.05);
+    text-align:center;
+    border-radius: 8px;
+    z-index:99;
+`
+export const innerShell = css`
+    background: #EAEAEF;
+    text-align:center;
+    padding:20px;
+    border-radius: 8px;
+    padding-top:40px;
+    margin:4%;
+    max-height: 300px;
+`
+export const closeWrapper = css`
+    float: right;
+    width:4%;
+    font-size: 20px;
+    font-weight: bold;
+    color:#363B64;
+    cursor:pointer;
+    @media(max-width: ${mobileScreenWidth}){
+        font-size: 12px;
+    };
+`
+export const fileButton = css`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 9px 9px;
+    position: absolute;
+    width: 200px ;
+    height: 50px;
+    background-color: #363B64;
+    color:white;
+    border:none;
+    border-radius: 20px;
+    cursor:pointer;
+    margin-right:auto;
+`
+export const fileData = css`
+    width: 100%;
+    display: flex;
+    height: 60px;
+    justify-content: center;
+    margin-top:15px;
+`
