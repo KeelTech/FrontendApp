611f035795b3b38fbd3b30025b914fe7dfb6bd86
jayant arora <jayantarora011@gmail.com>
2021-08-03 12:51:33 +0530

    Update index.js

--- dev/js/pages/AgentViews/index.js
+++ dev/js/pages/AgentViews/index.js
@@ -8,7 +8,6 @@ import TaskView from './TaskView';
 import AgentTaskDetail from '@pages/AgentTaskDetail/TaskDetailMobileEntry.js';
 import CreateTaskMobileView from '@pages/CreateTaskMobileView';
 import CustomerView from './CustomerView';
-import CustomerInfoView from './CustomerView/CustomerInfoView';
 
 const UserDashboardView = ({ match }) => {
  
@@ -24,7 +23,6 @@ const UserDashboardView = ({ match }) => {
                     <Route exact path={`${match.path}/task/detail/:taskId`} component={AgentTaskDetail}/>
                     <Route exact path={`${match.path}/customer`} component={CustomerView}/>
                     <Route exact path={`${match.path}/customer/:caseId`} component={CustomerInfoView}/>
-
                 </Switch>
             </div>
             <FloatingChatWidget/>
@@ -32,4 +30,4 @@ const UserDashboardView = ({ match }) => {
     )
 }
 
-export default UserDashboardView;
+export default UserDashboardView; 
