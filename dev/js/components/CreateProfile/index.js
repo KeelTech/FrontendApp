c6814c6c2eedb6079d2a46b253863830d060344d
unknown <mayank.yadav@affle.com>
2021-08-21 12:11:09 +0530

    dashboard Update

--- dev/js/components/CreateProfile/index.js
+++ dev/js/components/CreateProfile/index.js
@@ -7,10 +7,10 @@ import CustomToaster from '@components/CustomToaster';
 import ProfileForm from './ProfileForm.js';
 import { container, progressBar } from './style.js';
 
-const CreateProfile = ()=>{
+const CreateProfile = () => {
     const dispatch = useDispatch();
-    const taskInfo = useSelector(state=>state.TASK_INFO);
-    const { fullProfileInfo, fullProfileLoading, userInfo={} } = taskInfo;
+    const taskInfo = useSelector(state => state.TASK_INFO);
+    const { fullProfileInfo, fullProfileLoading, userInfo = {} } = taskInfo;
     const isProfileExist = userInfo && userInfo.profile_exists;
     const [activeState, setActive] = useState(1);
     const [loading, setLoading] = useState(false);
@@ -22,92 +22,92 @@ const CreateProfile = ()=>{
         msg: ''
     })
 
-    const activeWidgetData = useMemo(()=>{
+    const activeWidgetData = useMemo(() => {
         let activeWidgetInfo = {
             widget: '',
             dataParams: {},
             displayText: ''
         }
-        if(!fullProfileLoading && fullProfileInfo.profile){
+        if (!fullProfileLoading && fullProfileInfo.profile) {
             const { profile, education_assessment, qualification, work_experience, relative_in_canada } = fullProfileInfo;
-            if(activeState===1){
+            if (activeState === 1) {
                 activeWidgetInfo = {
                     widget: 'profile',
-                    dataParams: {...profile},
+                    dataParams: { ...profile },
                     displayText: 'Personal Details'
                 }
-            }else if(activeState===2){
+            } else if (activeState === 2) {
                 activeWidgetInfo = {
                     widget: 'education_assessment',
                     dataParams: [...education_assessment],
                     displayText: 'Education',
                     isMultiple: true
                 }
-            }else if(activeState===3){
+            } else if (activeState === 3) {
                 activeWidgetInfo = {
                     widget: 'qualification',
                     dataParams: [...qualification],
                     displayText: 'Educational Creational Assessment',
                     isMultiple: true
                 }
-            }else if(activeState===4){
+            } else if (activeState === 4) {
                 activeWidgetInfo = {
                     widget: 'work_experience',
                     dataParams: [...work_experience],
                     displayText: 'Work Experience',
                     isMultiple: true
                 }
-            }else if(activeState===5){
+            } else if (activeState === 5) {
                 activeWidgetInfo = {
                     widget: 'relative_in_canada',
-                    dataParams: {...relative_in_canada},
+                    dataParams: { ...relative_in_canada },
                     displayText: 'Relative in Canada (if any)'
                 }
             }
         }
         return activeWidgetInfo;
-    },[activeState, fullProfileInfo, fullProfileLoading])
+    }, [activeState, fullProfileInfo, fullProfileLoading])
 
-    useEffect(()=>{
+    useEffect(() => {
         getFullUserProfile({}, dispatch);
-    },[])
+    }, [])
 
-    const handleFormNavigation = (isNext=false)=>{
-        if(isNext){
-            const { widget, dataParams, isMultiple=false } = activeWidgetData;
+    const handleFormNavigation = (isNext = false) => {
+        if (isNext) {
+            const { widget, dataParams, isMultiple = false } = activeWidgetData;
 
             let isError = false;
             let newDataParams = {};
 
-            if(isMultiple){
-                newDataParams=[];
+            if (isMultiple) {
+                newDataParams = [];
                 let subFieldItems = {};
-                dataParams.map((subField, subIndex)=>{
+                dataParams.map((subField, subIndex) => {
                     newDataParams[subIndex] = {};
-                    Object.entries(subField).map((val, key)=>{
+                    Object.entries(subField).map((val, key) => {
                         const [fieldType, dataValues] = val;
                         const { value, labels } = dataValues;
-                        if(!labels) return;
+                        if (!labels) return;
                         let showError = false;
-                        if(!value){
+                        if (!value) {
                             isError = true;
                             showError = true;
                         }
-                        subFieldItems[fieldType] = {...dataValues, showError};
+                        subFieldItems[fieldType] = { ...dataValues, showError };
                     })
                     newDataParams.push(subFieldItems);
                 })
-            }else{
-                Object.entries(dataParams).map((val, key)=>{
+            } else {
+                Object.entries(dataParams).map((val, key) => {
                     const [fieldType, dataValues] = val;
                     const { value, labels } = dataValues;
-                    if(!labels) return;
+                    if (!labels) return;
                     let showError = false;
-                    if(!value){
+                    if (!value) {
                         isError = true;
                         showError = true;
                     }
-                    newDataParams[fieldType]= {...dataValues, showError }
+                    newDataParams[fieldType] = { ...dataValues, showError }
                 })
             }
             let updatedParams = {
@@ -115,33 +115,33 @@ const CreateProfile = ()=>{
                 type: widget,
                 isUpdate: isMultiple
             }
-            if(isError){
+            if (isError) {
                 updateUserProfile(updatedParams, dispatch);
                 return;
             };
-            if(activeState==5){
+            if (activeState == 5) {
                 handleCreateForm();
-            }else{
-                setActive(val=>val+1);
+            } else {
+                setActive(val => val + 1);
             }
-        }else{
-            setActive(val=>val-1);
+        } else {
+            setActive(val => val - 1);
         }
     }
 
-    const handleCreateForm = ()=>{
+    const handleCreateForm = () => {
         setLoading(true);
-        if(isProfileExist){
-            updateProfile(fullProfileInfo, dispatch, (resp, err)=>{
+        if (isProfileExist) {
+            updateProfile(fullProfileInfo, dispatch, (resp, err) => {
                 setLoading(false);
-                if(resp){
+                if (resp) {
                     setToasterInfo({
                         isVisible: true,
                         isError: false,
                         isSuccess: true,
                         msg: 'Profile Updated Successfully'
                     });
-                }else{
+                } else {
                     setToasterInfo({
                         isVisible: true,
                         isError: true,
@@ -156,16 +156,16 @@ const CreateProfile = ()=>{
             return;
         }
 
-        createFullUserProfile(fullProfileInfo, dispatch, (resp, err)=>{
+        createFullUserProfile(fullProfileInfo, dispatch, (resp, err) => {
             setLoading(false);
-            if(resp){
+            if (resp) {
                 setToasterInfo({
                     isVisible: true,
                     isError: false,
                     isSuccess: true,
                     msg: 'Profile Created Successfully'
                 });
-            }else{
+            } else {
                 setToasterInfo({
                     isVisible: true,
                     isError: true,
@@ -179,59 +179,143 @@ const CreateProfile = ()=>{
         });
     }
 
-    const hideToaster = ()=>{
+    const hideToaster = () => {
         setToasterInfo({
             isVisible: false
         })
     }
 
-    const { widget, dataParams, displayText, isMultiple=false } = activeWidgetData;
-    return(
+    const { widget, dataParams, displayText, isMultiple = false } = activeWidgetData;
+    return (
         <div className={container}>
             {
-                loading || fullProfileLoading?<div className={loaderView}><LoadingWidget/></div>:null
+                loading || fullProfileLoading ? <div className={loaderView}><LoadingWidget /></div> : null
             }
-            <CustomToaster {...toasterInfo} hideToaster={hideToaster}/>
+            <CustomToaster {...toasterInfo} hideToaster={hideToaster} />
             <div className={progressBar}>
-            <div className="desktopProgressBar">
-                <div className="leftPorgressBar">
-                    <ul class="progressbar">
-                        <li className={activeState===1?'active':''}></li>
-                        <li className={activeState===2?'active':''}></li>
-                        <li className={activeState===3?'active':''}></li>
-                        <li className={activeState===4?'active':''}></li>
-                        <li className={activeState===5?'active':''}></li>
-                    </ul>
-                </div>
-                <div className="userFormsMainContainer">
-                    <h3>{displayText}</h3>
-                    <div className="formsScroll">
-                        {
-                            isMultiple?
-                            dataParams.map((subField, subIndex)=>{
-                                return Object.entries(subField).map((val, key)=>{
-                                    const [fieldType, dataValues] = val;
-                                    return <ProfileForm fieldType={fieldType} dataParams={dataValues} key={`${widget}_${key}`} widget={widget} subIndex={subIndex} isMultiple/>
-                                })
-                            })
-                            :Object.entries(dataParams).map((val, key)=>{
-                                const [fieldType, dataValues] = val;
-                                return <ProfileForm fieldType={fieldType} dataParams={dataValues} key={`${widget}_${key}`} widget={widget}/>
-                            })
-                        }
+                <div className="desktopProgressBar">
+                    <div className="leftPorgressBar">
+                        <ul class="progressbar">
+                            <li className={activeState === 1 ? 'active' : ''}></li>
+                            <li className={activeState === 2 ? 'active' : ''}></li>
+                            <li className={activeState === 3 ? 'active' : ''}></li>
+                            <li className={activeState === 4 ? 'active' : ''}></li>
+                            <li className={activeState === 5 ? 'active' : ''}></li>
+                        </ul>
                     </div>
-                    <div className="btnCont">
-                        {
-                            activeState>1?<button onClick={()=>handleFormNavigation(false)}>Previous</button>:null
-                        }
-                        {
-                            activeState==5?<button onClick={()=>handleFormNavigation(true)}>{isProfileExist?'Update':'Create'}</button>
-                            :<button onClick={()=>handleFormNavigation(true)}>Next</button>
-                        }
+                    <div className="userFormsMainContainer">
+                        <h3>{displayText}</h3>
+                        <div className="formsScroll">
+                            {
+                                isMultiple ?
+                                    dataParams.map((subField, subIndex) => {
+                                        return Object.entries(subField).map((val, key) => {
+                                            const [fieldType, dataValues] = val;
+                                            return <ProfileForm fieldType={fieldType} dataParams={dataValues} key={`${widget}_${key}`} widget={widget} subIndex={subIndex} isMultiple />
+                                        })
+                                    })
+                                    : Object.entries(dataParams).map((val, key) => {
+                                        const [fieldType, dataValues] = val;
+                                        return <ProfileForm fieldType={fieldType} dataParams={dataValues} key={`${widget}_${key}`} widget={widget} />
+                                    })
+                            }
+                        </div>
+                        <div className="btnCont">
+                            {
+                                activeState > 1 ? <button onClick={() => handleFormNavigation(false)}>Previous</button> : null
+                            }
+                            {
+                                activeState == 5 ? <button onClick={() => handleFormNavigation(true)}>{isProfileExist ? 'Update' : 'Create'}</button>
+                                    : <button onClick={() => handleFormNavigation(true)}>Next</button>
+                            }
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+            <div className="useDetailsContainer">
+                <div className="userProfile">
+                    <div className="userNameDtls">
+                        <img className="img-fluid" src={ASSETS_BASE_URL + "/images/common/user.svg"} />
+                        <h3>Mayank Yadav</h3>
+                    </div>
+                    <button className="editPrfl">Edit Profile</button>
+                </div>
+                <div className="editProgress">
+                    <label>30% complete</label>
+                    <div className="progressBar">
+                        <div className="progBarFill" style={{ width: '30%' }}></div>
+                    </div>
+                </div>
+                <div className="prflDtlsAccordionContainer">
+                    <div className="prfAccrd">
+                        <div className="accrdHead">
+                            <h5>Personal  Details</h5>
+                            <button>Hide Details <img className="img-fluid" src={ASSETS_BASE_URL + "/images/common/drop.svg"} /></button>
+                        </div>
+                        <div className="accrdContent">
+                            <ul>
+                                <li>
+                                    <h5>Email:</h5>
+                                    <p>samantha.williams@gmail.com</p>
+                                </li>
+                                <li>
+                                    <h5>Number:</h5>
+                                    <p>lorem Sed ut perspiciatis, unde omnis iste natus</p>
+                                </li>
+                                <li>
+                                    <h5>Date of Birth:</h5>
+                                    <p>lorem Sed ut perspiciatis, unde omnis iste natus</p>
+                                </li>
+                                <li>
+                                    <h5>Place of Birth:</h5>
+                                    <p>lorem Sed ut perspiciatis, unde omnis iste natus</p>
+                                </li>
+                                <li>
+                                    <h5>City:</h5>
+                                    <p>lorem Sed ut , unde omnis iste natus</p>
+                                </li>
+                                <li>
+                                    <h5>Country:</h5>
+                                    <p>lorem  ut perspiciatis, unde omnis iste natus</p>
+                                </li>
+                                <li>
+                                    <h5>Nationality:</h5>
+                                    <p>lorem Sed ut perspiciatis, unde omnis iste natus</p>
+                                </li>
+                                <li>
+                                    <h5>National Identity Number:</h5>
+                                    <p>lorem Sed ut , unde omnis iste natus</p>
+                                </li>
+                                <li>
+                                    <h5>Date of Issue:</h5>
+                                    <p>lorem Sed ut perspiciatis,  omnis iste natus</p>
+                                </li>
+                                <li>
+                                    <h5>Date of Expiry:</h5>
+                                    <p>lorem Sed ut perspiciatis, unde omnis iste natus</p>
+                                </li>
+                                <li>
+                                    <h5>Passport Number:</h5>
+                                    <p> Sed ut perspiciatis, unde omnis iste natus</p>
+                                </li>
+                                <li>
+                                    <h5>Passport Number:</h5>
+                                    <p>lorem Sed ut , unde omnis iste natus</p>
+                                </li>
+                                <li>
+                                    <h5>Passport Number:</h5>
+                                    <p>lorem Sed ut perspiciatis, unde omnis iste natus</p>
+                                </li>
+                                <li>
+                                    <h5>Current Address:</h5>
+                                    <p>lorem Sed ut perspiciatis, unde omnis iste natus</p>
+                                </li>
+
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
