88df1a149da25ba6480d0d3108b5c1fce557eda0
Pankajkeel <pankaj@getkeel.com>
2021-08-30 19:09:05 +0530

    added agent name, user id & case id in chat , chat in customer detail

--- dev/js/pages/AgentViews/CustomerView/CustomerInfoView/index.js
+++ dev/js/pages/AgentViews/CustomerView/CustomerInfoView/index.js
@@ -1,6 +1,7 @@
 import React,{useEffect} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import FloatingChatWidget from '@components/FloatingChatWidget';
 import NotificationWidget from '@components/NotificationWidget';
 import ProfileWidget from '@components/ProfileWidget';
 import ChatWidget from '@components/ChatWidget';
@@ -14,6 +15,11 @@ function CustomerInfoView(props) {
   const dispatch = useDispatch();
   const taskInfo = useSelector((store) => store.TASK_INFO);
   const { caseDetails, caseDetailLoading } = taskInfo || {};
+
+  // const agentInfo = useSelector(state=>state.AGENT_STORE);
+  // const { agentProfile={} } = agentInfo;
+  // const { agent_profile={} } = agentProfile;
+  // const { id='' } = agent_profile;
 
   let caseId = '';
   if(props && props.match && props.match.params){
@@ -39,7 +45,7 @@ function CustomerInfoView(props) {
     task_count = null,
   } = caseDetails;
 
-  const { first_name, last_name} = user_details;
+  const { first_name, last_name, id } = user_details;
   const { display_id, plan} = case_details;
   return (
     <div className={body}>
@@ -56,61 +62,67 @@ function CustomerInfoView(props) {
           <LoadingWidget />
         </div>
       ) : (
-        <React.Fragment>
-          <div className="basicInfo">
-            <div className="infoWrapper">
-              {/* <img className="userImage" /> */}
-              <div className="userInfoHeaderWrapper">
-                <h3 className="userName">
-                  {first_name} {last_name} (#{display_id})
-                </h3>
-                <div className="flexWrapper">
-                  <img
-                    className="tagImg"
-                    src={ASSETS_BASE_URL + "/images/common/tagIcon.svg"}
-                    alt="list"
-                  />
-                  <p className="visaPackage">{plan}</p>
-                </div>
-                <div className="flexWrapper">
-                  <img
-                    className="listImg"
-                    src={ASSETS_BASE_URL + "/images/common/list.svg"}
-                    alt="list"
-                  />
-                  <p className="taskDetails">{task_count} Task Pending for review</p>
+        <div className="main-container">
+          <div className="leftBar">
+            <div className="basicInfo">
+              <div className="infoWrapper">
+                {/* <img className="userImage" /> */}
+                <div className="userInfoHeaderWrapper">
+                  <h3 className="userName">
+                    {first_name} {last_name}
+                  </h3>
+                  <div className="flexWrapper">
+                    <img
+                      className="tagImg"
+                      src={ASSETS_BASE_URL + "/images/common/tagIcon.svg"}
+                      alt="list"
+                    />
+                    <p className="visaPackage">{plan}</p>
+                  </div>
+                  <div className="flexWrapper">
+                    <img
+                      className="listImg"
+                      src={ASSETS_BASE_URL + "/images/common/list.svg"}
+                      alt="list"
+                    />
+                    <p className="taskDetails">{task_count} Task Pending for review</p>
+                  </div>
                 </div>
               </div>
+              <div className="buttonWrapper">
+                <button className="taskButton" onClick={redirectToTask}>Tasks</button>
+                {/* <button className="docButton">Documents</button> */}
+              </div>
             </div>
-            <div className="buttonWrapper">
-              <button className="taskButton" onClick={redirectToTask}>Tasks</button>
-              {/* <button className="docButton">Documents</button> */}
+            {/* <div className="meetingInfoWrapper">
+              <div className="meetingInfoFlexWrapper">
+                <div className="meetingTextWrapper">
+                  <p className="meetingHeader">This title is for the meeting</p>
+                  <div className="meetingDateWrapper">
+                    <img
+                      className="calendarImg"
+                      src={ASSETS_BASE_URL + "/images/common/calendarIcon.svg"}
+                      alt="calender"
+                    />
+                    <p className="meetingDate">8:30pm, 28th March, 2021</p>
+                  </div>
+                </div>
+                <div className="meetingBtnWrapper">
+                  <button className="joinBtn">Join Meeting</button>
+                  <button className="scheduleBtn">Reschedule</button>
+                </div>
+              </div>
+            </div> */}
+            <div className="completeInfoWrapper">
+              <InfoList info={caseDetails}/>
             </div>
           </div>
-          {/* <div className="meetingInfoWrapper">
-            <div className="meetingInfoFlexWrapper">
-              <div className="meetingTextWrapper">
-                <p className="meetingHeader">This title is for the meeting</p>
-                <div className="meetingDateWrapper">
-                  <img
-                    className="calendarImg"
-                    src={ASSETS_BASE_URL + "/images/common/calendarIcon.svg"}
-                    alt="calender"
-                  />
-                  <p className="meetingDate">8:30pm, 28th March, 2021</p>
-                </div>
-              </div>
-              <div className="meetingBtnWrapper">
-                <button className="joinBtn">Join Meeting</button>
-                <button className="scheduleBtn">Reschedule</button>
-              </div>
-            </div>
-          </div> */}
-          <div className="completeInfoWrapper">
-            <InfoList info={caseDetails}/>
+          <div className="chat">
+              {caseId && id ? <ChatWidget caseId={caseId} currentUserId={id} /> : ""}
           </div>
-        </React.Fragment>
+        </div>
       )}
+      {caseId && id ? <FloatingChatWidget caseId={caseId} currentUserId={id}/>:""}
     </div>
   );
 }
