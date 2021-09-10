568e7bc4234ed7243ab784cf3f5f6d262b2bfd81
unknown <mayank.yadav@affle.com>
2021-09-10 10:47:57 +0530

    mobile view customer

--- dev/js/components/ChatWidget/index.js
+++ dev/js/components/ChatWidget/index.js
@@ -7,7 +7,7 @@ import { sendChatMessage, getChatMessages } from '@actions';
 import { MERGE_CHAT_MESSAGES } from '@constants/types';
 
 
-const ChatWidget = ({ floatingChat=false, toggleChat, caseId="", currentUserId="", chatHeaderName="" })=>{
+const ChatWidget = ({ floatingChat = false, toggleChat, caseId = "", currentUserId = "", chatHeaderName = "" }) => {
     //console.log(currentUserId)
     const dispatch = useDispatch();
     const mainClass = cx({
@@ -16,46 +16,46 @@ const ChatWidget = ({ floatingChat=false, toggleChat, caseId="", currentUserId="
     })
 
     // chat data mappers
-    const chatData = useSelector(state=>state.CHAT);
+    const chatData = useSelector(state => state.CHAT);
     const [inputMessage, setinputMessage] = useState("");
-    
+
     // inital chat setup, this code will run only one time
-    useEffect(()=>{
+    useEffect(() => {
         let checkChat = null
-        if(caseId){
-            getChatMessages(caseId, {limit:1000, offset:0}, dispatch)
+        if (caseId) {
+            getChatMessages(caseId, { limit: 1000, offset: 0 }, dispatch)
             checkChat = setInterval(() => {
-                getChatMessages(caseId, {limit:1000, offset:0}, dispatch)
+                getChatMessages(caseId, { limit: 1000, offset: 0 }, dispatch)
             }, 2000)
             setTimeout(scrollToBottom, 1000)
         }
         return () => {
             dispatch({
                 type: MERGE_CHAT_MESSAGES,
-                payload: { caseId:null, messages: [] }
+                payload: { caseId: null, messages: [] }
             })
-            if(checkChat){
+            if (checkChat) {
                 clearInterval(checkChat)
             }
         }
-    },[caseId])
+    }, [caseId])
 
     // chat mobile/desktop handler
-    useEffect(()=>{
-        if(floatingChat){
-            document.body.style.overflow="hidden";
+    useEffect(() => {
+        if (floatingChat) {
+            document.body.style.overflow = "hidden";
         }
         return () => {
-            document.body.style.overflow="";
+            document.body.style.overflow = "";
         }
-    },[floatingChat])
+    }, [floatingChat])
 
     // chat input handlers
     const sendButtonHandler = () => {
-        if(inputMessage && inputMessage.length < 256) {
-            sendChatMessage({"message": inputMessage, "case_id": caseId}, (res,err) => {
-                if(caseId){
-                    getChatMessages(caseId, {limit:10, offset:0}, dispatch)
+        if (inputMessage && inputMessage.length < 256) {
+            sendChatMessage({ "message": inputMessage, "case_id": caseId }, (res, err) => {
+                if (caseId) {
+                    getChatMessages(caseId, { limit: 10, offset: 0 }, dispatch)
                 }
                 setinputMessage("")
             })
@@ -68,12 +68,12 @@ const ChatWidget = ({ floatingChat=false, toggleChat, caseId="", currentUserId="
     }
 
     // this fx tries to maintain the scroll position in chat window
-    useEffect(()=>{
+    useEffect(() => {
         maintainScrollPosition()
-    },[chatData.chatMessages])
+    }, [chatData.chatMessages])
     const maintainScrollPosition = () => {
         let elem = document.getElementById("chatScrollableWindow")
-        if(elem){
+        if (elem) {
             // 100px is assumed to be the heigh of a message
             if (elem.scrollHeight - elem.scrollTop - elem.clientHeight < 100) {
                 scrollToBottom()
@@ -82,16 +82,16 @@ const ChatWidget = ({ floatingChat=false, toggleChat, caseId="", currentUserId="
     }
     const scrollToBottom = () => {
         let elem = document.getElementById("chatScrollableWindow")
-        if(elem){
+        if (elem) {
             elem.scrollTop = elem.scrollHeight - elem.clientHeight
         }
     }
 
-    return(
+    return (
         <div className={mainClass + " " + "chatMainContainer"}>
             <div className="header d-flex align-items-center">
                 <div className="profile">
-                <img className="img-fluid" src={ASSETS_BASE_URL+"/images/common/Avatar_blue.svg"} alt="user" />
+                    <img className="img-fluid" src={ASSETS_BASE_URL + "/images/common/Avatar_blue.svg"} alt="user" />
                 </div>
                 <div className="nameSection">
                     <div className="name">{chatHeaderName}</div>
@@ -100,19 +100,19 @@ const ChatWidget = ({ floatingChat=false, toggleChat, caseId="", currentUserId="
                         <span className="status">Online</span>
                     </div> */}
                 </div>
-                <img className="close" src={ASSETS_BASE_URL+"/images/common/crossIcon.svg"} alt="close" onClick={toggleChat}/>
+                <img className="close" src={ASSETS_BASE_URL + "/images/common/crossIcon.svg"} alt="close" onClick={toggleChat} />
             </div>
-            <div className={chatWidget({floatingChat}) + " " + "forMobileChat"} id="chatScrollableWindow">
-                <ChatCard floatingChat={floatingChat} messages={chatData.chatMessages} currentUserId={currentUserId}/>
+            <div className={chatWidget({ floatingChat }) + " " + "forMobileChat"} id="chatScrollableWindow">
+                <ChatCard floatingChat={floatingChat} messages={chatData.chatMessages} currentUserId={currentUserId} />
             </div>
-            <div className={msgWidget({floatingChat})}>
+            <div className={msgWidget({ floatingChat })}>
                 <div className="chatbox">
-                    <input type="text" className="" placeholder="Write your message..." value={inputMessage} onKeyDown={handleKeyDown} onChange={(e)=>setinputMessage(e.target.value)}/>
+                    <input type="text" className="" placeholder="Write your message..." value={inputMessage} onKeyDown={handleKeyDown} onChange={(e) => setinputMessage(e.target.value)} />
                     <div className="sendWidget">
                         {/* <img className="attachment" src={ASSETS_BASE_URL+"/images/common/attachment.svg"} alt="attachment"/> */}
                         <div className="sendBtn" onClick={sendButtonHandler}>
                             <span></span>
-                            <img className="icon" src={ASSETS_BASE_URL+"/images/common/sendIcon.svg"} alt="send"/>
+                            <img className="icon" src={ASSETS_BASE_URL + "/images/common/sendIcon.svg"} alt="send" />
                         </div>
                     </div>
                 </div>
