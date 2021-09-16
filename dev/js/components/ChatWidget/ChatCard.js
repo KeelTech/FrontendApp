fa9b41b3af2a9d7da36c02949443946f719908eb
princetp634 <pankaj@getkeel.com>
2021-09-16 13:37:21 +0530

    sender name chat

--- dev/js/components/ChatWidget/ChatCard.js
+++ dev/js/components/ChatWidget/ChatCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { getNameInitialHelper } from '@helpers/utils';
 import { chatBox } from './style.js';
 
 const ChatCard = ({ floatingChat = false, messages = [], currentUserId="" })=>{
@@ -7,13 +8,16 @@ const ChatCard = ({ floatingChat = false, messages = [], currentUserId="" })=>{
         <div className={chatBox({floatingChat})}>
             {
                 messages.map((val, key)=>{
-                    let { isReceiver=true, message, created_at, sender } = val;
+                    let { isReceiver=true, message, created_at, sender, sender_name='' } = val;
                     isReceiver = currentUserId != sender
                     created_at = (new Date(created_at)).toLocaleString()
                     return (
                         <div className={isReceiver?'receiver':'sender'} key={key}>
                             <div className="msg">
                                 <span className="chatMsg">{message}</span>
+                                {
+                                    sender_name?<span className="profile">{getNameInitialHelper(sender_name)}</span>:null
+                                }
                                 {
                                     created_at?<span className="msgTime">{created_at}</span>:null
                                 }
