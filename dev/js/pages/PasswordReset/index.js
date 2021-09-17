6572c7cddc8b683ece96767cbc4f70fb4528eafd
unknown <mayank.yadav@affle.com>
2021-09-17 19:35:04 +0530

    userupdate

--- dev/js/pages/PasswordReset/index.js
+++ dev/js/pages/PasswordReset/index.js
@@ -77,70 +77,108 @@ function PasswordReset(props) {
           <LoadingWidget />
         </div>
       )}
-      <div className={style}>
-        <div className="container">
-          <img
+      <div className={'loginMainContainer' + ' ' + style}>
+        <div className="row no-gutters fullHeight">
+          <div className="col-md-6 col-12">
+            <div className="loginContainer">
+              <div className="loginContent">
+                <div className="loginLogo">
+                  <img
+                    className="keel-logo img-fluid"
+                    src={ASSETS_BASE_URL + '/images/common/keelIcon.svg'}
+                    alt="keel-logo"
+                    onClick={() => logoClick()}
+                  />
+                </div>
+                <h3 className="header-text">Reset Password</h3>
+
+                {showInputFields && (
+                  <form className="form-wrapper" onSubmit={submitHandler}>
+                    <div className="formCont">
+                      <label>Enter OTP</label>
+                      <input
+                        placeholder="Enter OTP"
+                        type="text"
+                        value={otp}
+                        onChange={otpHandler}
+                        autoComplete="off"
+                      />
+                    </div>
+                    <div className="formCont">
+                      <label>New Password</label>
+                      <input
+                        placeholder="New Password"
+                        type="password"
+                        value={password}
+                        onChange={passwordHandler}
+                        autoComplete="off"
+                      />
+                    </div>
+                    <div className="formCont">
+                      <label>Confirm New Password</label>
+                      <input
+                        placeholder="Confirm New Password"
+                        type="password"
+                        value={confirmPassword}
+                        onChange={confirmPasswordHandler}
+                        autoComplete="off"
+                      />
+                    </div>
+                    {passwordValidation && (
+                      <p className="password-match">Passwords do not match</p>
+                    )}
+                    <button className="reset-pass-button log-in-button">Reset Password</button>
+                  </form>
+                )}
+                {successMessage && (
+                  <Fragment>
+                    <p className="password-success">
+                      New Password has been successfully set up. Redirecting to login
+                      page in <span className="timer">{timer}</span> seconds
+                    </p>
+                    <p className="emailSuccessBtnMsg">
+                      Or you can click the button below
+                    </p>
+                    <Link to="/" className="BtnLink">
+                      <button className="LinkBtn">Login</button>
+                    </Link>
+                  </Fragment>
+                )}
+                {errorMessage && (
+                  <p className="errorMsg">
+                    Failed to authenticate OTP, either OTP is invalid or timed out.
+                  </p>
+                )}
+              </div>
+              {/* <img
+                  className="globe-image"
+                  src={ASSETS_BASE_URL + '/images/Login/globe-image.jpeg'}
+                  alt="globe"
+                /> */}
+            </div>
+          </div>
+          <div className="col-md-6 col-12 mobileHide">
+            <div className="loginRightImg">
+              <img
+                className=" img-fluid"
+                src={ASSETS_BASE_URL + '/images/Login/login_high_res.png'}
+                alt="pasport-image"
+              />
+              <h4>Want to immigrate to<br /> Canada?</h4>
+              <p>All in one immigration Platform</p>
+            </div>
+
+          </div>
+        </div>
+        {/* <img
             className="keel-logo"
             src={ASSETS_BASE_URL + '/images/common/keelIcon.svg'}
             alt="keel-logo"
             onClick={() => logoClick()}
           />
-          <h1 className="header-text">Reset Password</h1>
+          <h1 className="header-text">Reset Password</h1> */}
 
-          {showInputFields && (
-            <form className="form-wrapper" onSubmit={submitHandler}>
-              <input
-                placeholder="Enter OTP"
-                type="text"
-                value={otp}
-                onChange={otpHandler}
-                autoComplete="off"
-              />
-              <input
-                placeholder="New Password"
-                type="password"
-                value={password}
-                onChange={passwordHandler}
-                autoComplete="off"
-              />
-              <input
-                placeholder="Confirm New Password"
-                type="password"
-                value={confirmPassword}
-                onChange={confirmPasswordHandler}
-                autoComplete="off"
-              />
-              {passwordValidation && (
-                <p className="password-match">Passwords do not match</p>
-              )}
-              <button className="reset-pass-button">Reset Password</button>
-            </form>
-          )}
-          {successMessage && (
-            <Fragment>
-              <p className="password-success">
-                New Password has been successfully set up. Redirecting to login
-                page in <span className="timer">{timer}</span> seconds
-              </p>
-              <p className="emailSuccessBtnMsg">
-                Or you can click the button below
-              </p>
-              <Link to="/" className="BtnLink">
-                <button className="LinkBtn">Login</button>
-              </Link>
-            </Fragment>
-          )}
-          {errorMessage && (
-            <p className="errorMsg">
-              Failed to authenticate OTP, either OTP is invalid or timed out.
-            </p>
-          )}
-        </div>
-        <img
-          className="globe-image"
-          src={ASSETS_BASE_URL + '/images/Login/globe-image.jpeg'}
-          alt="globe"
-        />
+
       </div>
     </Fragment>
   );
