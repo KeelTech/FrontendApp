f665532280db33f6fe21762d9226adb0195e89c2
princetp634 <pankaj@getkeel.com>
2021-11-18 17:49:26 +0530

    added code for redirection

--- dev/js/components/NotificationView/index.js
+++ dev/js/components/NotificationView/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import Header from '@components/Header';
 import { css } from '@emotion/css';
@@ -21,6 +22,20 @@ export const body = css`
 const NotificationView = ()=>{
     const taskInfo = useSelector(state=>state.TASK_INFO);
     const { notificationList=[] } = taskInfo;
+    const history = useHistory();
+
+    const clickHandler = (val)=>{
+        const { category, case_id } = val;
+        if(category=='TASKS'){
+            history.push('/tasks');
+        }else if(category=='DOCUMENT'){
+            history.push('/vault');
+        }else if(category=='CHATS'){
+            history.push('/');
+        }else if(category=='HOME'){
+            history.push('/');
+        }
+    }
 
     return(
         <div className={body + '    ' + 'p-relative pt-5 dashTaskSchSection '}>
@@ -29,8 +44,8 @@ const NotificationView = ()=>{
                 <div className="pushNotification">
                 {
                     notificationList.map((val)=>{
-                    const { id, seen, text } = val;
-                    return <div className={`pushCards ${seen?'':'clickedPush'}`} key={id}>
+                    const { id, seen, text, category } = val;
+                    return <div className={`pushCards ${seen?'':'clickedPush'}`} key={id} onClick={()=>clickHandler(val)}>
                     <div className="icoContent">
                         <div className="notifyIcon">
                         <img className="img-fluid" src={ASSETS_BASE_URL + "/images/common/video.svg"} alt="video" />
--- dev/js/components/NotificationWidget/NotificationDropdown.js
+++ dev/js/components/NotificationWidget/NotificationDropdown.js
@@ -1,10 +1,26 @@
 import React from "react";
 import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import { notification, header, content, content__list, message } from "./style";
 
 const NotificationDropdown = () => {
   const taskInfo = useSelector(state=>state.TASK_INFO);
   const { notificationList=[] } = taskInfo;
+
+  const history = useHistory();
+
+  const clickHandler = (val)=>{
+      const { category, case_id } = val;
+      if(category=='TASKS'){
+          history.push('/tasks');
+      }else if(category=='DOCUMENT'){
+          history.push('/vault');
+      }else if(category=='CHATS'){
+          history.push('/');
+      }else if(category=='HOME'){
+          history.push('/');
+      }
+  }
 
   return (
     <div className={notification}>
@@ -19,7 +35,7 @@ const NotificationDropdown = () => {
           {
             notificationList.map((val)=>{
               const { id, seen, text } = val;
-              return <div className={`pushCards ${seen?'':'clickedPush'}`} key={id}>
+              return <div className={`pushCards ${seen?'':'clickedPush'}`} key={id} onClick={()=>clickHandler(val)}>
               <div className="icoContent">
                 <div className="notifyIcon">
                   <img className="img-fluid" src={ASSETS_BASE_URL + "/images/common/video.svg"} alt="video" />
