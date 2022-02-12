7dd9f4e47270e203aa344543356dc22b75b48fba
princetp634 <pankaj@getkeel.com>
2022-02-13 01:17:27 +0530

    addec

--- dev/js/pages/UserDashboardView/ApplicationStatus/index.js
+++ dev/js/pages/UserDashboardView/ApplicationStatus/index.js
@@ -44,7 +44,10 @@ const ApplicationStatus = ()=>{
                                                     <input type="checkbox" checked />
                                                     <span className="checkmark"></span>
                                                 </Fragment>
-                                                :null
+                                                :<Fragment>
+                                                <input type="checkbox" />
+                                                <span className="checkmark"></span>
+                                            </Fragment>
                                             }
                                         </label>
                                     </div>
