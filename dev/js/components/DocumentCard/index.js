76bcfee913e9d07587d483c9aabc1c2340e4a5a1
prince634 <pankaj@getkeel.com>
2023-01-17 20:57:53 +0530

    added profile name

--- dev/js/components/DocumentCard/index.js
+++ dev/js/components/DocumentCard/index.js
@@ -44,7 +44,7 @@ const DocumentCard = ({ documentData, downloadDocumentClicked, deleteDocumentCli
             </div>
             {/* <div className="docOverlay"></div> */}
             {
-                showMenuBar?<DocumentModal id={id} docId={doc_id} toggle={toggleMenuBar} downloadDocumentClicked={downloadDocumentClicked} deleteDocumentClicked={deleteDocumentClicked}/>:null
+                showMenuBar?<DocumentModal id={id} docId={doc_id} toggle={toggleMenuBar} orignal_file_name={orignal_file_name} downloadDocumentClicked={downloadDocumentClicked} deleteDocumentClicked={deleteDocumentClicked}/>:null
             }
         </div>
     )
